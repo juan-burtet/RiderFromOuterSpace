175306575ca418b2fb21552ffdd8e33535f7256c
Juan Burtet <juan.burtet@gmail.com>
2018-04-16 12:04:45 -0300

    Tech Demo

--- Player.gd
+++ Player.gd
@@ -1,63 +1,24 @@
-extends Area2D
+extends KinematicBody2D
 
-export (int) var SPEED  # how fast the player will move (pixels/sec)
-var screensize  # size of the game window
+const UP = Vector2(0,-1)
+const GRAVITY = 20
+const SPEED = 200
+const JUMP_HEIGHT = -500
 
-signal hit
+var motion = Vector2()
 
-func _ready():
-	screensize = get_viewport_rect().size
+func _physics_process(delta):
+	motion.y += GRAVITY
 	
-
-func _process(delta):
-	var velocity = Vector2() # the player's movement vector
-	if Input.is_action_pressed("ui_down"):
-		velocity.y += 1
-	if Input.is_action_pressed("ui_up"):
-		velocity.y -= 1
 	if Input.is_action_pressed("ui_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("ui_left"):
-		velocity.x -= 1
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * SPEED
-		$AnimatedSprite.play()
+		motion.x = SPEED
+	elif Input.is_action_pressed("ui_left"):
+		motion.x = - SPEED
 	else:
-		$AnimatedSprite.stop()
-		
-	position += velocity * delta
-	position.x = clamp(position.x, 0, screensize.x)
-	position.y = clamp(position.y, 0, screensize.y)
-		
-	if velocity.x != 0:
-		$AnimatedSprite.animation = "right"
-		$AnimatedSprite.flip_v = false
-		$AnimatedSprite.flip_h = velocity.x < 0
-	elif velocity.y != 0:
-		$AnimatedSprite.animation = "up"
-		$AnimatedSprite.flip_v = velocity.y > 0
-
-func _on_Player_body_entered(body):
-	hide()
-	emit_signal("hit")
-	$CollisionShape2D.disabled = true
-
-func start(pos):
-	position = pos
-	show()
-	$CollisionShape2D.disabled = false
-
-func game_over():
-	$ScoreTimer.stop()
-	$MobTimer.stop()
-	$HUD.show_game_over()
-
-func new_game():
-	score = 0
-	$Player.start($StartPosition.position)
-	$StartTimer.start()
-	$HUD.update_score(score)
-	$HUD.show_message("Get Ready")
-
-func _on_HUD_start_game():
-	pass # replace with function body
+		motion.x = 0
+	
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_up"):
+			motion.y = JUMP_HEIGHT
+	
+	motion = move_and_slide(motion, UP)
